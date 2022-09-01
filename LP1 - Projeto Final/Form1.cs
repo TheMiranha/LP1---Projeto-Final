74c389f7fa23505de7e26d97a998e277a5fbc95a
TheMiranha <lxbrmiranda@gmail.com>
2022-09-01 16:54:59 -0300

    Adicionar arquivos de projeto.

---
+++ LP1 - Projeto Final/Form1.cs
@@ -0,0 +1,18 @@
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace LP1___Projeto_Final
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+    }
+}
