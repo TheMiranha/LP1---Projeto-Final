59e80d4383e7566c2224335d75d1331db5ae3b4d
themiranha <lxbrmiranda@gmail.com>
2022-09-02 09:12:24 -0300

    Ajustando o tamanho da tela de notas

--- LP1 - Projeto Final/Notas/FNotas.Designer.cs
+++ LP1 - Projeto Final/Notas/FNotas.Designer.cs
@@ -64,7 +64,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1511, 651);
+            this.ClientSize = new System.Drawing.Size(745, 651);
             this.Controls.Add(this.LP);
             this.Controls.Add(this.btnAdd);
             this.Name = "FNotas";
